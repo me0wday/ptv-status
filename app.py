431787bde5086d3ea5aee1d5ee13f91ae482ae82
Victor <vk@VKMBP.local>
2016-09-15 19:52:37 +1000

    test4

--- app.py
+++ app.py
@@ -35,5 +35,9 @@ def index():
 				final_string += "\n"
 	return final_string
 
+if __name__ == '__main__':
+        import os  
+        port = int(os.environ.get('PORT', 5000)) 
+        app.run(host='0.0.0.0', port=port)
 	
     
