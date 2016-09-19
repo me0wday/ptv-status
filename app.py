22ed38b267821f44fd12260a869fea9dd845faae
Victor <vk@VKMBP.local>
2016-09-19 11:33:34 +1000

    test

--- app.py
+++ app.py
@@ -1,53 +1,26 @@
-from bs4 import BeautifulSoup
 import os
+import slackweb
 from flask import Flask, request
+from utils import get_ptv_status
 app = Flask(__name__)
+slack = slackweb.Slack(url=os.environ.get('slackhook'))
 
 import requests
 
 @app.route('/')
 def index():
 	if (request.args.get('token') == os.environ.get('token')):
-		try:
-			r  = requests.get("https://www.ptv.vic.gov.au/live-travel-updates/")
+		status = get_ptv_status()
+		return status
+	else:
+		return "Error"
 
-			if not r.status_code // 100 == 2:
-				return "Error: Unexpected response"
-
-			data = r.text
-			soup = BeautifulSoup(data)
-			if (request.args.get('slackpost') == "bot"):
-				final_string = ""
-			else:	
-				final_string = "*Current status of Victorian train lines:* \n"
-			for div in soup.find_all('div', class_='LineInfo'):
-					line = div.find('div', class_='titleHolder')
-					status = div.find('span', class_='bubbleType')
-					more_info = soup.find(id=('article-' + div.get('data-id')))
-
-					if ((line == None) or (status == None)):
-						continue
-
-					final_string += "*" +div.find('div', class_='titleHolder').text + "* | "
-					if "Good" in status.text:
-						final_string += ":green_heart: "
-					elif "Major" in status.text:
-						final_string += ":red_circle: "
-					elif "Minor" in status.text:
-						final_string += ":large_orange_diamond: "
-					elif "Part" in status.text:
-						final_string += ":black_circle: "
-
-					final_string += div.find('span', class_='bubbleType').text
-
-					if not (more_info == None):
-						final_string += " | " + soup.find(id=('article-' + div.get('data-id'))).text + "\n"
-					else:
-						final_string += "\n"
-			return final_string
-		except requests.exceptions.RequestException as e:
-			#return "Error: {}".format(e)
-			return "Error"
+@app.route('/hook')
+def index():
+	if (request.args.get('token') == os.environ.get('token')):
+		status = get_ptv_status()
+		slack.notify(text=status)
+		return "Success"
 	else:
 		return "Error"
 
---
+++ utils.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+
+def get_ptv_status()
+	try:
+		r  = requests.get("https://www.ptv.vic.gov.au/live-travel-updates/")
+
+		if not r.status_code // 100 == 2:
+			return "Error: Unexpected response"
+
+		data = r.text
+		soup = BeautifulSoup(data)
+		final_string = "*Current status of Victorian train lines:* \n"
+		for div in soup.find_all('div', class_='LineInfo'):
+				line = div.find('div', class_='titleHolder')
+				status = div.find('span', class_='bubbleType')
+				more_info = soup.find(id=('article-' + div.get('data-id')))
+
+				if ((line == None) or (status == None)):
+					continue
+
+				final_string += "*" +div.find('div', class_='titleHolder').text + "* | "
+				if "Good" in status.text:
+					final_string += ":green_heart: "
+				elif "Major" in status.text:
+					final_string += ":red_circle: "
+				elif "Minor" in status.text:
+					final_string += ":large_orange_diamond: "
+				elif "Part" in status.text:
+					final_string += ":black_circle: "
+
+				final_string += div.find('span', class_='bubbleType').text
+
+				if not (more_info == None):
+					final_string += " | " + soup.find(id=('article-' + div.get('data-id'))).text + "\n"
+				else:
+					final_string += "\n"
+		return final_string
+	except requests.exceptions.RequestException as e:
+		#return "Error: {}".format(e)
+		return "Error"
